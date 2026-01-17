96cc56f14acbbce0645b865388187232b8ee1ad8
LAPTOP-90O3I2F7\yulik <yulikeren3@gmail.com>
2026-01-17 18:19:42 +0200

    initial commit

---
+++ app/src/main/java/com/game/tictactoe1/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.game.tictactoe1
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button = findViewById<Button>(R.id.main_start_button)
+
+        button.setOnClickListener{
+            val intent = android.content.Intent(this, StartActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
